56ff7ee64bcaaa5376d0f149402f2eb40a4601b4
Muazzum Akhtar <muazzum.akhtar@peopleshores.com>
2024-10-17 08:23:43 -0400

    create dtos

--- src/main/java/theater/project/MovieTheater/DTO/User/CreateCustomerResponseDTO.java
+++ src/main/java/theater/project/MovieTheater/DTO/User/CreateCustomerResponseDTO.java
@@ -4,14 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import theater.project.MovieTheater.Enum.UserRole;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CreateAdminRequestDTO {
-    private String name;
-    private UserRole userRole = UserRole.ADMIN;
+public class CreateCustomerResponseDTO {
 }
-
